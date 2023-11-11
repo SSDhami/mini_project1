d11ff80476531f2de74214ba3f47c64471cac58a
sat <satinderjit.99@gmail.com>
2023-11-11 16:29:35 -0700

    adding rock paper scissors

---
+++ rock_paper_scissors.py
@@ -0,0 +1,28 @@
+#Import “Random” library.
+import random
+  
+    
+#Assign a random play to the computer from a list of options (rock, paper, or scissors).
+options =["rock","paper","scissors"]
+comp_result = random.choice(options)
+    
+#Take the input from the player.
+player_input = input("Enter your choice(from rock, paper, scissors):  ")
+    
+#Print an error message to the user if the input is incorrect.
+#Compare the player’s input to the computer’s.
+#Print the game result to the user.
+if player_input == "rock" or player_input == "paper" or player_input == "scissors":
+    if player_input == comp_result:
+        print(comp_result,", tie!!!!")
+    else:
+        if player_input == "rock" and comp_result == "paper" :
+            print(comp_result,", You Lost!!!!")
+        elif player_input == "scissors" and comp_result == "rock":
+            print(comp_result,", You Lost!!!!")
+        elif player_input == "paper" and comp_result == "scissors":
+            print(comp_result,", You Lost!!!!")
+        else: 
+            print(comp_result,", You Won!!!!")
+else:
+    print("Input is incorrect!!!")
